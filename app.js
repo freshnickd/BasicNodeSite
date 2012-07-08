dc38fd2963d67e8b6ffd3476c1e572e8ec13b742
Nick Durland <nickdurland@Nicks-MacBook-Pro.local>
2012-07-07 22:21:15 -0400

    Added "ss" to print out when server runs.

--- app.js
+++ app.js
@@ -37,5 +37,5 @@ app.get('/', store.home);
 app.post('/', store.home_post_handler);
 
 app.listen(3000, function(){
-  console.log("Expre server listening on port %d in %s mode", app.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
